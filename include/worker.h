0694e50728e8d96941ba50b5ea7bf1b5596af48e
jordyaraujo <jordyaraujo@outlook.com>
2018-07-31 11:12:56 -0300

    Added some comments

--- include/worker.h
+++ include/worker.h
@@ -6,10 +6,15 @@
 
 class Worker{
 public:
-	std::string name;
+	// The label that will be displayed in the game
+	std::string label;
+	//Worker's level
 	int level;
+	//Amount of workers
 	int amount;
+	//workers eficiency
 	int eficiency;
+	// This represents the cost to train a warrior (or buy, or whatever :P)
 	Resource * cost;
 
 	Worker(std::string, int, int, int, Resource *);
--- src/worker.cpp
+++ src/worker.cpp
@@ -1,6 +1,6 @@
 #include "worker.h"
 
-Worker::Worker(std::string n, int l, int a, int e, Resource *c) : name(n), level(l), amount(a), eficiency(e), cost(c){
+Worker::Worker(std::string n, int l, int a, int e, Resource *c) : label(label), level(level), amount(amount), eficiency(eficiency), cost(cost){
 
 }
 
